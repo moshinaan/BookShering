219fb7446f8ed77642ee2df6cbea9019726b3fe5
moshinaan <moshinahacker@yandex.ru>
2019-09-09 21:16:55 +0400

    update table for new books

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,7 @@ class Book < ApplicationRecord
   validates :name, presence: true#, uniqueness: { case_sensitive: false }
 
   #validates :author_id,  presence: true
-  #validates :year_publishing,  presence: true,  length: { maximum: 4 }
+  validates :year_publishing,  presence: true
+  year_publishing = 0000..9999
   #validates :age_restrictions,  presence: true,  length: { maximum: 2 }
 end
