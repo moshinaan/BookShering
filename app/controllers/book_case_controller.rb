f0cbf809fe3dffd506a89c93fd0922feec2b555b
moshinaan <moshinahacker@yandex.ru>
2019-10-03 01:05:27 +0400

    хуйня блять какая то

--- app/controllers/book_case_controller.rb
+++ app/controllers/book_case_controller.rb
@@ -1,14 +1,11 @@
 class BookCaseController < ApplicationController
 	before_action :authenticate, only: [:new, :create]
 	def new
-	  @bookcase = CollectionBookcasesForm.new Bookcase.new
+	  
 	end
 
 	def create
-	  @bookcase = CollectionBookcasesForm.new Bookcase.new
-	  params[:bookcase][:user_id] = current_user.id
-	  params[:bookcase] = 1
-	  params[:bookcase][:taken_at] = DateTime.now
+	  @bookcase = Bookcase.new(user_id: 1 , book_id: 1 , taken_at: Time.now)
 	end
 
 	
