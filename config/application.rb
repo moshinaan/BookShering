04eb0c839872e206ae765c00d4f20279e9477279
moshinaan <moshinahacker@yandex.ru>
2019-10-15 20:05:34 +0400

    update css

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Bookshering
 
     config.i18n.available_locales = [:en, :ru]
     config.i18n.enforce_available_locales = false
-	config.i18n.default_locale = :ru
+	  config.i18n.default_locale = :ru
     # Setting	s in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
