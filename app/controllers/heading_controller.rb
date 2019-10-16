9cef266dd305e4fbd881e668dea079a7d4fd1dfc
moshinaan <moshinahacker@yandex.ru>
2019-10-17 02:50:48 +0400

    heading/show

--- app/controllers/heading_controller.rb
+++ app/controllers/heading_controller.rb
@@ -1,7 +1,14 @@
 class HeadingController < ApplicationController
+
 	def index
-      @headings = Heading.all.map.pluck(:name) do |h|
+      @headings = Heading.all.map.pluck(:name, :id) do |h|
       	HeadingDecorator.new  h
       end
+	end
+
+	def show
+	  @books = Book.where(heading_id:  params[:id]).map do |h|
+	  	BookDecorator.new  h
+	  end
 	end
 end
