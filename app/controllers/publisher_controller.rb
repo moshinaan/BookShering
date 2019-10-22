e644b5bc01d7c1828f5ebcac388578b1cefaad46
moshinaan <moshinahacker@yandex.ru>
2019-10-22 07:38:00 +0400

    поиск книг по издательствуЭ exit

--- app/controllers/publisher_controller.rb
+++ app/controllers/publisher_controller.rb
@@ -1,2 +1,13 @@
 class PublisherController < ApplicationController
+  def index
+	@publishers = Publisher.all.map.pluck(:name, :id) do |h|
+	  PublisherDecorator.new h
+	end
+  end
+
+  def show
+	@books = Book.where(publisher_id:  params[:id]).map do |h|
+	  BookDecorator.new  h
+    end
+  end
 end
