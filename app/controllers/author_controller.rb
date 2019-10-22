58220b045e3ab3c58f74178725f14dea5341417f
moshinaan <moshinahacker@yandex.ru>
2019-10-22 06:56:26 +0400

    выбор книг по автору

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,4 +1,17 @@
 class AuthorController < ApplicationController
+	
+	def index
+	  @authors = Author.all.map.pluck(:name, :id) do |h|
+	  	AuthorDecorator.new h
+	  end
+	end
+
+	def show
+	  @books = Book.where(author_id:  params[:id]).map do |h|
+	  	BookDecorator.new  h
+	  end
+	end
+		
 	def new
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   resources :users
   resources :heading
   resources :publisher 
+  resources :author
 
 end
