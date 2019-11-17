656d405904a3a0f458163608ded03819e872398d
moshinaan <moshinahacker@yandex.ru>
2019-11-17 18:15:41 +0400

    fix welcome/index

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -2,7 +2,7 @@ class Web::WelcomeController < Tramway::Landing::ApplicationController
   layout 'tramway/landing/application'
 
   def index
-    @blocks = ::Tramway::Landing::BlockDecorator.decorate ::Tramway::Landing::Block.on_main_page
+    #@blocks = ::Tramway::Landing::BlockDecorator.decorate ::Tramway::Landing::Block.on_main_page
     @links = []
   end
 end
